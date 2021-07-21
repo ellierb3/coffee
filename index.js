87910e40b6a0d733ec9e53fa4324e49ba78cfe96
ellierb3 <erbeel01@louisville.edu>
2021-07-20 20:04:53 -0700

    added javascript guessing game element on main page

--- index.js
+++ index.js
@@ -26,6 +26,19 @@ submit.onclick = function() {
     alert('Testing');
 }
 
+submitform.onclick = function() {
+    alert('Thanks for signing up for our Newsletter');
+}
+
+
+function submitformpls() {
+    var x = document.getElementById("email").required;
+  }
+
+submitformpls.onclick = function() {
+      alert("Git it!");
+  }
+
 
 const suggest = document.querySelector('#suggest');
 const heading = document.querySelector('#heading');
@@ -33,4 +46,23 @@ suggest.onclick = function () {
     let question =prompt('What should we add to the menu?');
     alert('You suggested ' + question + ', check back for menu updates soon!');
     heading.textContent = 'Thanks for your input';
+}
+
+
+
+function bagel() {
+    var bageltype = document.getElementById("guessWhich").value;
+    var text;
+
+    if (bageltype === "blueberry"){
+        text = "Yes! Blueberry is the best seller!";
+    }
+    else if (bageltype === "cinnamon raisin"|| bageltype === "everything"){
+        text = "Good try but nope!";
+    }
+    else {
+        text = "Hey that wasnt an option.."
+    }
+    document.getElementById("tester").innerHTML = text;
+
 }
