7fc7324227d2d4e565f7a1ac902d7ffafccaf8c1
ellierb3 <erbeel01@louisville.edu>
2021-07-21 18:53:13 -0700

    Added comments

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+//suggestion box
+
 const suggest = document.querySelector('#suggest');
 
 suggest.onclick = function () {
     let question =prompt('Got a crazy suggestion? Let us know!');
     alert('You suggested ' + question + '! Check back for menu updates soon!');
+    if (question === null|| question === '') {
+        alert('Let us know if you think of something!');
+    }
 }
 
-
+//picture that changes when you click it
 
 const happy = document.querySelector('#happy');
 
@@ -20,13 +25,13 @@ function changeImage() {
 }
 
 
-
+//function for main page guessing game
 
 function bagel() {
     var bageltype = document.getElementById("guessWhich").value;
     var text;
 
-    if (bageltype === "blueberry"){
+    if (bageltype === "blueberry"|| bageltype === "Blueberry"){
         text = "Yes! Blueberry is the best seller!";
     }
     else if (bageltype === "cinnamon raisin"|| bageltype === "everything"){
